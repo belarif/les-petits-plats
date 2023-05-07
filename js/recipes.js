bc582c65be9019f18130cfd110f28d99260d0540
Belarif <b.ocine@live.fr>
2023-05-07 20:42:43 +0200

    LPP1: creating ingredient filter boutton

--- js/recipes.js
+++ js/recipes.js
@@ -9,4 +9,45 @@ function displayRecipesData() {
     });
 }
 
+const dropdownUlElt = document.querySelector(".dropdown-ul");
+const dropdownElt = document.querySelector(".dropdown-wrapper");
+const filterLabelElt = document.querySelector(".dropdown-label");
+const chevronUpElt = document.querySelector(".bi-chevron-up");
+const chevronDownElt = document.querySelector(".bi-chevron-down");
+const searchIngredientElt = document.querySelector(".search-ingredient");
+const chevronsElt = document.querySelector(".chevrons");
+
+function openDropdownList() {
+    dropdownUlElt.style.opacity = "1";
+    dropdownElt.style.borderRadius = "5px 5px 0 0";
+    chevronDownElt.style.display = "none";
+    chevronUpElt.style.display = "block";
+    searchIngredientElt.style.display = "block";
+    filterLabelElt.style.display = "none";
+}
+
+function closeDropdownList() {  
+    dropdownUlElt.style.opacity = "0";
+    dropdownElt.style.borderRadius = "5px";
+    chevronDownElt.style.display = "block";
+    chevronUpElt.style.display = "none";
+    filterLabelElt.style.display = "block";
+    searchIngredientElt.style.display = "none";
+}
+
+function navigateOnFilterWithMouse() {
+    let direction = false;
+    chevronsElt.addEventListener("click", () => {
+        if (direction === false) {
+            openDropdownList();
+            direction = true;
+        } else {
+            closeDropdownList();
+            direction = false;
+        }
+    });
+}
+
+navigateOnFilterWithMouse();
+
 displayRecipesData();
