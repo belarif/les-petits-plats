f31fea19344d0d17ff930821351e762f4ec97e7f
Belarif <b.ocine@live.fr>
2023-05-08 14:01:52 +0200

    LPP1: creating appliance filter dropdown

--- js/recipes.js
+++ js/recipes.js
@@ -9,45 +9,69 @@ function displayRecipesData() {
     });
 }
 
-const dropdownUlElt = document.querySelector(".dropdown-ul");
-const dropdownElt = document.querySelector(".dropdown-wrapper");
-const filterLabelElt = document.querySelector(".dropdown-label");
-const chevronUpElt = document.querySelector(".bi-chevron-up");
-const chevronDownElt = document.querySelector(".bi-chevron-down");
+// ingredient
+const dropdownIngredientUlElt = document.querySelector(".dropdown-ul-ingredient");
+const dropdownIngredientElt = document.querySelector(".dropdown-wrapper-ingredient");
+const chevronsIngredientElt = document.querySelector(".chevrons-ingredient");
+const filterIngredientLabelElt = document.querySelector(".dropdown-label-ingredient");
 const searchIngredientElt = document.querySelector(".search-ingredient");
-const chevronsElt = document.querySelector(".chevrons");
+const chevronUpIngredientElt = document.querySelector(".chevron-up-ingredient");
+const chevronDownIngredientElt = document.querySelector(".chevron-down-ingredient");
 
-function openDropdownList() {
+// appliance
+const dropdownApplianceUlElt = document.querySelector(".dropdown-ul-appliance");
+const dropdownApplianceElt = document.querySelector(".dropdown-wrapper-appliance");
+const chevronsApplianceElt = document.querySelector(".chevrons-appliance");
+const filterApplianceLabelElt = document.querySelector(".dropdown-label-appliance");
+const searchApplianceElt = document.querySelector(".search-appliance");
+const chevronUpApplianceElt = document.querySelector(".chevron-up-appliance");
+const chevronDownApplianceElt = document.querySelector(".chevron-down-appliance");
+
+function openDropdownList(dropdownElt, dropdownUlElt, filterLabelElt, searchElt, chevronUpElt, chevronDownElt) {
     dropdownUlElt.style.opacity = "1";
     dropdownElt.style.borderRadius = "5px 5px 0 0";
     chevronDownElt.style.display = "none";
     chevronUpElt.style.display = "block";
-    searchIngredientElt.style.display = "block";
+    searchElt.style.display = "block";
     filterLabelElt.style.display = "none";
 }
 
-function closeDropdownList() {  
+function closeDropdownList(dropdownElt, dropdownUlElt, filterLabelElt, searchElt, chevronUpElt, chevronDownElt) {  
     dropdownUlElt.style.opacity = "0";
     dropdownElt.style.borderRadius = "5px";
     chevronDownElt.style.display = "block";
     chevronUpElt.style.display = "none";
     filterLabelElt.style.display = "block";
-    searchIngredientElt.style.display = "none";
+    searchElt.style.display = "none";
 }
 
-function navigateOnFilterWithMouse() {
+function navigateIngredientDropdown() {
     let direction = false;
-    chevronsElt.addEventListener("click", () => {
+    chevronsIngredientElt.addEventListener("click", () => {
         if (direction === false) {
-            openDropdownList();
+            openDropdownList(dropdownIngredientElt, dropdownIngredientUlElt, filterIngredientLabelElt, searchIngredientElt, chevronUpIngredientElt, chevronDownIngredientElt);
             direction = true;
         } else {
-            closeDropdownList();
+            closeDropdownList(dropdownIngredientElt, dropdownIngredientUlElt, filterIngredientLabelElt, searchIngredientElt, chevronUpIngredientElt, chevronDownIngredientElt);
             direction = false;
         }
     });
 }
 
-navigateOnFilterWithMouse();
+function navigateApplianceDropdown() {
+    let direction = false;
+    chevronsApplianceElt.addEventListener("click", () => {
+        if (direction === false) {
+            openDropdownList(dropdownApplianceElt, dropdownApplianceUlElt, filterApplianceLabelElt, searchApplianceElt, chevronUpApplianceElt, chevronDownApplianceElt);
+            direction = true;
+        } else {
+            closeDropdownList(dropdownApplianceElt, dropdownApplianceUlElt, filterApplianceLabelElt, searchApplianceElt, chevronUpApplianceElt, chevronDownApplianceElt);
+            direction = false;
+        }
+    });
+}
+
+navigateIngredientDropdown();
+navigateApplianceDropdown();
 
 displayRecipesData();
