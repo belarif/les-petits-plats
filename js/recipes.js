815e6a50d8e28d6205fe2c1b351d123e8f68d4da
Belarif <b.ocine@live.fr>
2023-05-12 16:59:09 +0200

    LPP3: setting ingredients in dropdown filter

--- js/recipes.js
+++ js/recipes.js
@@ -9,4 +9,28 @@ function displayRecipesData() {
     });
 }
 
+function getIngredients() {
+    let ingredients = [];
+    recipes.forEach((recipe) => {
+        ingredients = [
+            ...ingredients,
+            ...recipe.ingredients.map((ingredient) => ingredient.ingredient),
+          ]; 
+    });
+
+    const finalIngredients = new Set (ingredients);
+    return Array.from(finalIngredients);
+}
+
+function setIngredientsInDropdown() {
+    const ingredients = getIngredients();
+    ingredients.forEach(ingredient => {
+        const ingredientUlElt = document.querySelector(".ingredient .dropdown-ul");
+        const ingredientLiElt = document.createElement("li");
+        ingredientLiElt.textContent = ingredient;
+        ingredientUlElt.appendChild(ingredientLiElt);
+    })
+}
+
+setIngredientsInDropdown();
 displayRecipesData();
