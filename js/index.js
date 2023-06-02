82a562f1a3f5ceda7a01a5be1eca0a163c655d14
luc <luc.leonardi@opsonet.com>
2023-06-02 11:02:35 +0200

    fix  ingredient filter

--- js/index.js
+++ js/index.js
@@ -1,75 +1,74 @@
 "use_strict";
 
 function setRecipes(recipes) {
-    const recipesOrderedByName = orderRecipesByName(recipes);
+  const recipesOrderedByName = orderRecipesByName(recipes);
 
-    recipesOrderedByName.forEach((recipe) => {
-        const recipeModel = recipeCardsFactory(recipe);
-        const recipeDOM = recipeModel.getRecipeDOM();
-        recipeCardElt.appendChild(recipeDOM);
-    });
+  recipesOrderedByName.forEach((recipe) => {
+    const recipeModel = recipeCardsFactory(recipe);
+    const recipeDOM = recipeModel.getRecipeDOM();
+    recipeCardElt.appendChild(recipeDOM);
+  });
 }
 
 /**
- * 
- * @param {object} recipes 
+ *
+ * @param {object} recipes
  * @returns {object}
  */
 function orderRecipesByName(recipes) {
-
-    return recipes.sort(function(a,b) { 
-        if (a.name < b.name) {
-            return -1;
-        } else if (a.name > b.name) {
-            return 1;
-        } else {
-            return 0;
-        }
-    });
+  return recipes.sort(function (a, b) {
+    if (a.name < b.name) {
+      return -1;
+    } else if (a.name > b.name) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
 }
 
 /**
- * 
- * @param {object} searchedRecipes 
+ *
+ * @param {object} searchedRecipes
  */
 function refreshRecipes(searchedRecipes) {
-    rowCardElt.innerHTML = "";
-    setRecipes(searchedRecipes);
+  rowCardElt.innerHTML = "";
+  setRecipes(searchedRecipes);
 }
 
 /**
- * 
- * @param {object} searchedRecipes 
+ *
+ * @param {object} searchedRecipes
  */
 function refreshDropdowns(searchedRecipes) {
-    ingredientUlElt.innerHTML = "";
-    const updatedIngredients = getUpdatedIngredients(searchedRecipes);
-    displayItemsDropdown(updatedIngredients, ingredientUlElt);
+  ingredientUlElt.innerHTML = "";
+  const updatedIngredients = getUpdatedIngredients(searchedRecipes);
+  displayItemsDropdown(updatedIngredients, ingredientUlElt);
 
-    applianceUlElt.innerHTML = "";
-    const updatedApplicances = getUpdatedAppliances(searchedRecipes);
-    displayItemsDropdown(updatedApplicances, applianceUlElt); 
+  applianceUlElt.innerHTML = "";
+  const updatedApplicances = getUpdatedAppliances(searchedRecipes);
+  displayItemsDropdown(updatedApplicances, applianceUlElt);
 
-    utensilUlElt.innerHTML = "";
-    const updatedUtensils = getUpdatedUtensils(searchedRecipes);
-    displayItemsDropdown(updatedUtensils, utensilUlElt);
+  utensilUlElt.innerHTML = "";
+  const updatedUtensils = getUpdatedUtensils(searchedRecipes);
+  displayItemsDropdown(updatedUtensils, utensilUlElt);
 }
 
 function init() {
-    const allRecipes = recipes;
+  const allRecipes = recipes;
 
-    setRecipes(allRecipes);
-    setIngredientsInDropdown();
-    setAppliancesInDropdown();
-    setUtensilsInDropdown();
+  setRecipes(allRecipes);
+  setIngredientsInDropdown();
+  setAppliancesInDropdown();
+  setUtensilsInDropdown();
 
-    searchRecipesInIngredientsBar();
-    searchRecipesInAppliancesBar();
-    searchRecipesInUtensilsBar();
+  searchRecipesInIngredientsBar();
+  searchRecipesInAppliancesBar();
+  searchRecipesInUtensilsBar();
 
-    filterByIngredients();
-    filterByAppliances();
-    filterByUtensils();
+  filterByIngredients();
+  filterByAppliances();
+  filterByUtensils();
 }
 
 init();
