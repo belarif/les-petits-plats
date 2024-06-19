b95233689e2025f4acba2a638db0afdf27693b77
Belarif <b.ocine@live.fr>
2024-06-19 16:08:06 +0200

    les-petits-plats-prod: adding modal that displays tools used to develop les-petits-plats app

--- js/index.js
+++ js/index.js
@@ -83,4 +83,14 @@ function init() {
   filterByUtensils();
 }
 
+// modal-tools
+document.addEventListener("DOMContentLoaded", () => {
+  const modal = document.querySelector(".modal-tools");
+  const modalBtn = document.querySelector(".close-modal-tools button");
+
+  modalBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+  });
+});
+
 init();
